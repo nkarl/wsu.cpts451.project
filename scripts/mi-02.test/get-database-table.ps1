8a78706ea513ecc2d42991b6894fc551e5d0091c
nkarl <21993921+nkarl@users.noreply.github.com>
2023-04-03 18:24:28 -0700

    Updated.

--- scripts/mi-02.test/get-database-table.ps1
+++ scripts/mi-02.test/get-database-table.ps1
@@ -1,3 +1,3 @@
-docker exec -it cpts-451-database psql yelpdb postgres -c "SELECT * FROM business;"
-docker exec -it cpts-451-database psql yelpdb postgres -c "SELECT * FROM review;"
-docker exec -it cpts-451-database psql yelpdb postgres -c "SELECT * FROM checkin;"
+docker exec -it cpts-451-database psql yelpdb -U postgres -c "SELECT * FROM business;"
+docker exec -it cpts-451-database psql yelpdb -U postgres -c "SELECT * FROM review;"
+docker exec -it cpts-451-database psql yelpdb -U postgres -c "SELECT * FROM checkin;"
--- scripts/mi-02/create-database.ps1
+++ scripts/mi-02/create-database.ps1
@@ -1,2 +1,2 @@
-docker exec -it cpts-451-database psql -U postgres -c "CREATE DATABASE yelpdb;"
+#docker exec -it cpts-451-database psql -U postgres -c "CREATE DATABASE yelpdb;"
 docker exec -it cpts-451-database psql yelpdb postgres -f "/database/CharlesNguyen_relations_v2.sql"
