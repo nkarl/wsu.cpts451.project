4b2a4c1cf23973cae42cd67dc18f4b22482be978
nkarl <21993921+nkarl@users.noreply.github.com>
2023-05-01 18:26:09 -0700

    Updated commits.

--- scripts/mi-02.test/get-database-info.ps1
+++ scripts/mi-02.test/get-database-info.ps1
@@ -1,2 +1,2 @@
 docker exec -it cpts-451-database psql -U postgres -c '\l'
-docker exec -it cpts-451-database psql yelpdb postgres -c '\dt'
+docker exec -it cpts-451-database psql yelpdb postgres -c '\dt+'
